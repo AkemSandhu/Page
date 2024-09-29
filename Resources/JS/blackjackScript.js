213f992c64416f83269a57a66e463abafcb5440b
Akem Sandhu <akemsandhu@outlook.com>
2024-09-29 11:24:12 -0400

    Weekend portion finished

--- Resources/JS/blackjackScript.js
+++ Resources/JS/blackjackScript.js
@@ -1,123 +1,99 @@
+
 let money = 500;
-   //main deck
-   let deck = [];
-   //player hand
-   let player = [];
-   //dealer hand
-   let dealer = [];
-   updateMoney()
-   startGame();
-   //starts the game automatically
-   function startGame(){
-       //clears the deck
-       deck = [];
-       player = [];
-       dealer = [];
-       //adds 52 cards to deck
-       for(let i = 0; i < 4; i++){
-           for(let j = 1; j < 14; j++){
-               deck.push(j);
-           }
-       }
-       shuffleArray(deck);
-       //draws starting cards
-       draw(player,deck);
-       draw(player,deck);
-       draw(dealer,deck);
-       draw(dealer,deck);
-       checkBust();
-  }
-  //after dealer is done drawing cards
-  function end(){
-       if(value(player)>21){player = [0]}
-       if(value(dealer)>21){dealer = [0]}
-       if(value(player)>value(dealer)){
-           money += 100; 
-           document.getElementById('win').innerHTML = "Win!";
-       }
-       else if(value(player) == value(dealer)){
-           money += 0;
-           document.getElementById('win').innerHTML = "Tie";
-       }
-       else{
-           money -= 100;
-           document.getElementById('win').innerHTML = "Lose...";
-       }
-       updateMoney();
-       var delayInMilliseconds = 1000; //1 second
-       setTimeout(function() {
-           startGame();
-       }, delayInMilliseconds);
-  }
-  //after you bust or finish drawing
-  function stand(){
-      
-       let bust = false;
-       //checks who won
-       if(value(player)>21){
-           bust = true;
-       }
-       while(value(dealer)<value(player) && !bust){
-           draw(dealer,deck)
-       }
-       if(value(dealer)==value(player)&&value(dealer)<17){
-           draw(dealer,deck)
-       }
-       end();
-  }
-  //returns value of hand
-  function value(hand){
-       let aces = hand.indexOf(1);
-       let x = 0;
-       for(let i = 0;i<hand.length;i++){
-           if(hand[i] >10){
-               x+=10;
-           }
-           else{
-           x+=hand[i]
-           }
-       }
-       //makes aces 1 or 11
-       if(x+10<=21 && aces > -1){
-           x+=10
-       }
-       return x;
-  }
+let deck = [];
+let player = [];
+let dealer = [];
+updateMoney();
+startGame();
 
+function startGame() {
+    deck = [];
+    player = [];
+    dealer = [];
+    for (let i = 0; i < 4; i++) {
+        for (let j = 1; j < 14; j++) {
+            deck.push(j);
+        }
+    }
+    shuffleArray(deck);
+    draw(player, deck);
+    draw(player, deck);
+    draw(dealer, deck);
+    draw(dealer, deck);
+    checkBust();
+}
 
-  function draw(array, drawpile){
-       //draws and updates text
-       array.push(drawpile.pop())
-       document.getElementById('playerHand').innerHTML = faceCardReplace("Player:" + player);
-       document.getElementById('playerScore').innerHTML = value(player);
-       document.getElementById('dealerHand').innerHTML = faceCardReplace("Dealer:" + dealer);
-       document.getElementById('dealerScore').innerHTML = value(dealer);
-       document.getElementById('win').innerHTML = "";
-  }
-  function checkBust(){
-       if(value(player)>21){stand();}
-  }
+function end() {
+    if (value(player) > 21) { player = [0]; }
+    if (value(dealer) > 21) { dealer = [0]; }
+    
+    if (value(player) > value(dealer)) {
+        money += 100;
+        document.getElementById('win').innerHTML = "Win!";
+    } else if (value(player) == value(dealer)) {
+        document.getElementById('win').innerHTML = "Tie";
+    } else {
+        money -= 100;
+        document.getElementById('win').innerHTML = "Lose...";
+    }
+    updateMoney();
+    
+    setTimeout(startGame, 1000);
+}
 
+function stand() {
+    let bust = false;
+    
+    if (value(player) > 21) {
+        bust = true;
+    }
+    
+    while (value(dealer) < 17 && !bust) {
+        draw(dealer, deck);
+    }
+    end();
+}
 
-   function shuffleArray(array){
-       //shuffles deck
-       for(let i = 0; i < array.length; i++){
-           let j = Math.floor(Math.random()*array.length);
-           let temp = array[i];
-           array[i] = array[j];
-           array[j] = temp;
-       }
-   }
-   function updateMoney(){
-       document.getElementById('money').innerHTML = "$"+money;
-   }
-function faceCardReplace(str){
-   //makes symbols instead of numbers for certain things
-    str = str.replace(/11/g,"J");
-    str = str.replace(/12/g,"Q");
-    str = str.replace(/13/g,"K");
-    str = str.replace(/1/g,"A");
-    //previous thing turns 10 into A0...
-    str = str.replace(/A0/g,"10");
-    return str;
+function value(hand) {
+    let aces = hand.filter(card => card === 1).length;
+    let score = hand.reduce((total, card) => {
+        return total + (card > 10 ? 10 : card);
+    }, 0);
+    
+    while (score <= 11 && aces > 0) {
+        score += 10;
+        aces--;
+    }
+    
+    return score;
+}
+
+function draw(array, drawpile) {
+    array.push(drawpile.pop());
+    document.getElementById('playerHand').innerHTML = faceCardReplace("Player: " + player.join(", "));
+    document.getElementById('playerScore').innerHTML = "Score: " + value(player);
+    document.getElementById('dealerHand').innerHTML = faceCardReplace("Dealer: " + dealer.join(", "));
+    document.getElementById('dealerScore').innerHTML = "Score: " + value(dealer);
+    document.getElementById('win').innerHTML = "";
+}
+
+function checkBust() {
+    if (value(player) > 21) {
+        stand();
+    }
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function updateMoney() {
+    document.getElementById('money').innerHTML = "Money: $" + money;
+}
+
+function faceCardReplace(str) {
+    return str.replace(/11/g, "J").replace(/12/g, "Q").replace(/13/g, "K").replace(/1/g, "A");
 }
--- Resources/JS/slotsScript.js
+++ Resources/JS/slotsScript.js
@@ -1,113 +1,74 @@
 let money = 500;
-       document.getElementById('money').innerHTML = "$"+money
-       let spinning = false;
-       //array of possible slot values, each individual of the 15 slots has these odds, and dont share a pool
-       const possibleValues = [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,7];
-       const slot = [[],[],[],[],[]];
-       let mult = 1;
-       function spin(bet){
-           //checks if slot is already spinning
-           if(spinning == false){
-               spinning = true;
-               mult = 1;
-               money -= bet
-               document.getElementById('money').innerHTML = "$"+money
-               random();
-               setTimeout(function(){
-                   //assigns values to slots
-                   for(let i = 0; i<5; i++){
-                       for(let j = 0; j < 3; j++){
-                           slot[i][j] = possibleValues[Math.floor(Math.random()*possibleValues.length)];
-                       }
-                   }
-                   //updates text
-                   document.getElementById('slot1').innerHTML = slot[0][0] + "  " + slot[1][0] + "  " + slot[2][0] + "  " + slot[3][0] + "  " + slot[4][0];
-                   document.getElementById('slot2').innerHTML = slot[0][1] + "  " + slot[1][1] + "  " + slot[2][1] + "  " + slot[3][1] + "  " + slot[4][1];
-                   document.getElementById('slot3').innerHTML = slot[0][2] + "  " + slot[1][2] + "  " + slot[2][2] + "  " + slot[3][2] + "  " + slot[4][2];
-                  
-                   //checking for matches
-                   for(let i = 0; i<3; i++){
-                       let row = i;
-                       let column = 0;
-                       let current = slot[row][column];
-                       adjacent(row,column,current);
-                   }
-                   //checking for sevens
-                   let sevens = 1;
-                   for(let i = 0; i<5; i++){
-                       for(let j = 0; j < 3; j++){
-                           if(slot[i][j]==7){
-                               sevens *= (sevens+1);
-                           }
-                       }
-                   }
-                   //highlight sevens
-                   document.getElementById('slot1').innerHTML = document.getElementById('slot1').innerHTML.replaceAll('7', '<span style="color: red;">'+7+'</span>');
-                   document.getElementById('slot2').innerHTML = document.getElementById('slot2').innerHTML.replaceAll('7', '<span style="color: red;">'+7+'</span>');
-                   document.getElementById('slot3').innerHTML = document.getElementById('slot3').innerHTML.replaceAll('7', '<span style="color: red;">'+7+'</span>');
-                  
-                   //increases money
-                   mult*=sevens;
-                   if(mult != 1){
-                       money += mult*bet;
-                   }
-                   //updates money
-                   document.getElementById('money').innerHTML = "$"+money
-                   //adds delay before respin
-                   setTimeout(function() {
-                       spinning = false;
-                   }, 500);
-               },3400)
-              
-           }
-           else{
+document.getElementById('money').innerHTML = "Money: $" + money;
+let spinning = false;
+const possibleValues = [1, 2, 3, 4, 5, 6, 7];
+const slot = [[], [], [], [], []];
+let mult = 1;
 
+function spin(bet) {
+    if (spinning || bet > money) {
+        return; // Prevent spinning if already in progress or not enough money
+    }
 
-           }
-      
-       function adjacent(row,column,current){
-           //checks for last row
-           if(column!=4){
-               //checks whether or not symbol is on the edge
-               let j = -1;
-               let k = 1;
-               if(row==0){j=0}
-               if(row==2){k=0}
-              
-               //checks for adjacent matches
-               for(i = j; i<=k; i++){
-                   if(slot[column+1][row+i] == slot[column][row]){
-                       adjacent(row+i,column+1,slot[column][row])
-                       };
-               }
-           }
-           //getting here means it got to the end succesfully
-           else{
-               //increases score based on how many correct symbols exist
-               multCount = 0
-               for(let i = 0; i<5; i++){
-                   for(let j = 0; j < 3; j++){
-                       if(slot[i][j]==current){
-                           multCount+=1
-                       }
-                   }
-               }
-               //increases money
-               mult *= multCount-3
-               //highlights correct symbols
-               document.getElementById('slot1').innerHTML = document.getElementById('slot1').innerHTML.replaceAll(''+current, '<span style="color: red;">'+current+'</span>');
-               document.getElementById('slot2').innerHTML = document.getElementById('slot2').innerHTML.replaceAll(''+current, '<span style="color: red;">'+current+'</span>');
-               document.getElementById('slot3').innerHTML = document.getElementById('slot3').innerHTML.replaceAll(''+current, '<span style="color: red;">'+current+'</span>');
-           }
-       }
-       function random(){
-           for(let i = 0; i < 45; i++){
-               //randomizes the slots to simulate spinning
-               setTimeout(() =>{
-                   document.getElementById('slot1').innerHTML = Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  ";
-                   document.getElementById('slot2').innerHTML = Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  ";
-                   document.getElementById('slot3').innerHTML = Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  " + Math.ceil(Math.random()*7) + "  ";
-               },i*75)
-           }
-       }
-       }
+    spinning = true;
+    mult = 1;
+    money -= bet;
+    document.getElementById('money').innerHTML = "Money: $" + money;
+
+    random(); // Start the random spinning animation
+    setTimeout(() => {
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 3; j++) {
+                slot[i][j] = possibleValues[Math.floor(Math.random() * possibleValues.length)];
+            }
+        }
+
+        updateSlots();
+        checkMatches(bet); // Check for matches and update money
+        spinning = false; // Allow new spin after 1 second
+    }, 3400);
+}
+
+function updateSlots() {
+    document.getElementById('slot1').innerHTML = slot.map(row => row[0]).join("  ");
+    document.getElementById('slot2').innerHTML = slot.map(row => row[1]).join("  ");
+    document.getElementById('slot3').innerHTML = slot.map(row => row[2]).join("  ");
+}
+
+function checkMatches(bet) {
+    let sevens = 1;
+
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (slot[i][j] === 7) {
+                sevens *= (sevens + 1);
+            }
+        }
+    }
+
+    // Highlight sevens
+    highlightSevens();
+
+    // Calculate winnings
+    mult *= sevens;
+    if (mult > 1) {
+        money += mult * bet;
+    }
+    document.getElementById('money').innerHTML = "Money: $" + money;
+}
+
+function highlightSevens() {
+    ['slot1', 'slot2', 'slot3'].forEach(slotId => {
+        document.getElementById(slotId).innerHTML = document.getElementById(slotId).innerHTML.replace(/7/g, '<span style="color: red;">7</span>');
+    });
+}
+
+function random() {
+    for (let i = 0; i < 45; i++) {
+        setTimeout(() => {
+            document.getElementById('slot1').innerHTML = Array.from({ length: 5 }, () => Math.ceil(Math.random() * 7)).join("  ");
+            document.getElementById('slot2').innerHTML = Array.from({ length: 5 }, () => Math.ceil(Math.random() * 7)).join("  ");
+            document.getElementById('slot3').innerHTML = Array.from({ length: 5 }, () => Math.ceil(Math.random() * 7)).join("  ");
+        }, i * 75);
+    }
+}
